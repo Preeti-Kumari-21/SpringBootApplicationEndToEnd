38fbda63799e1f0d015d3a67a0427b5bdcda1cd7
Preeti-Kumari-21 <preeti.kumari241116@gmail.com>
2026-04-28 06:39:22 +0530

    Order Service: added circuit breaker with fallback using Resilience4j

--- order-service/src/main/java/com/scaler/service/OrderService.java
+++ order-service/src/main/java/com/scaler/service/OrderService.java
@@ -6,12 +6,14 @@ import com.scaler.dto.OrderResponseDTO;
 import com.scaler.dto.UserResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 public class OrderService {
     @Autowired
     private UserClient userClient;
 
+    @CircuitBreaker(name="userService", fallbackMethod = "userServiceFallback")
     public OrderResponseDTO createOrder(int userId){
         UserResponseDTO userResponseDTO = userClient.getUserById(userId);
 
@@ -19,5 +21,13 @@ public class OrderService {
                 userResponseDTO.getId(),
                 userResponseDTO.getName(),
                 "Order created Successfully");
+    }
+
+    public OrderResponseDTO userServiceFallback(int userId , Exception ex){
+        return new OrderResponseDTO(
+                userId,
+                "Unavailable",
+                "User Service is temporarily unavailable, please try again later"
+        );
     }
 }
